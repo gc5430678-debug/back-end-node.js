947639923a7b68886c625599d40f843d027cea18
git config --global user.name <gc5430678@gmail.com>
2026-01-30 15:27:43 +0300

    first clean commit

--- home/uplodhome.js
+++ home/uplodhome.js
@@ -1,17 +1,6 @@
-const multer = require('multer')
-const path = require('path')
+const multer = require("multer");
 
-const storage = multer.diskStorage({
-    destination: (req, file, cp) => {
-        cp(null , "uplodhome/")
-    },
-    filename: (req, file, cp) => {
-        cp(null , Date.now() + path.extname(file.originalname))
-    }
+const storage = multer.memoryStorage();
+const uploadhome = multer({ storage });
 
-    
-})
-
-const uplodhome = multer({ storage })
-
-module.exports = uplodhome;
+module.exports = uploadhome;
